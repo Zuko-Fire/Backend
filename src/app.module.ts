fceebae3cd9ef6c38216b0b6f7009fc25908e18d
UsDaniil <us.daniil@yandex.ru>
2026-08-25 21:08:21 +0300

    feat(ci/cd): настройка ci/cd

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ import { AppService } from './app.service';
           autoLoadEntities: true, // ← Добавлено
           logging: nodeEnv === 'development',
           // Опционально: SSL для production
-          ssl: false,
+          ssl: nodeEnv === 'production' ? { rejectUnauthorized: false } : false,
         };
 
         // Валидация обязательных переменных
